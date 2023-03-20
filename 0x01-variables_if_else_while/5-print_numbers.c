4cb4c06637afab39d425d1c06688988bef682594
StellaMighty <stellaikang@gmail.com>
2023-03-20 15:19:42 -0700

    Print_Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - this is the entry function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
