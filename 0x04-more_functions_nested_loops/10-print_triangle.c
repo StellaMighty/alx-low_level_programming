9401f357183d94121a3570c6ff2e2b0693a5b39e
StellaMighty <stellaikang@gmail.com>
2023-03-24 18:25:17 -0700

    Printing Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * print_triangle - this function uses the character# to
+ * print out a triangle
+ * Return: always 0
+ */
+void print_triangle(int size)
+{
+	int rows, sign, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (rows = 1; rows <= size; rows++)
+		{
+			for (space = size - rows; space >= 1; space--)
+			{
+				_putchar(' ');
+			}
+			for (sign = 1; sign <= rows; sign++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
