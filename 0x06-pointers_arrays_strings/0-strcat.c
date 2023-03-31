1fad99e622bc612de858eda88fb7b115806003d6
StellaMighty <stellaikang@gmail.com>
2023-03-31 02:44:28 -0700

    concatenate 2 strings1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char string1[20];
-	char string2[20];
+
 	int i = 0;
 	int j = 0;
 
-	dest = string1;
-	src = string2;
 
-	while (string1[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		++dest;
 		i++;
 	}
-	while (string2[j] != '\0')
+	while (src[j] != '\0')
 	{
 		*dest = *src;
 		dest++;
