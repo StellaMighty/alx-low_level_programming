b8298d989f0ddca482811d22673a6a709d071693
StellaMighty <stellaikang@gmail.com>
2023-03-30 21:35:59 -0700

    header files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * *_strncat - this fubction concatenates strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of character to be displayed
+ *
+ * Return: 0 or 1
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+}
