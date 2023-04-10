51a53399f2bea037cbe5840087fbe85e945baa01
StellaMighty <stellaikang@gmail.com>
2023-04-10 02:25:07 -0700

    the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * main - theis is main function
+ * @argc: it counts the number of arguments
+ * @argv: this is a array of paramenters
+ *
+ * Return: always 0.
+ */
+
+int main(int __attribute__((__unused__))argc, char *argv[])
+{
+	printf("The name of the prog is: %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv__attribute__(unused))
+{
+	printf("the value of agrc is: %d\n", argc);
+		return (0);
+}
