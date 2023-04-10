635229ced93ae0e06ac2dc5c5d03aca2def658aa
StellaMighty <stellaikang@gmail.com>
2023-04-10 03:01:48 -0700

    the count of arguments in a program

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char* argv__attribute__(unused))
+/**
+ * main - this is the entry function
+ * @argc: this is the argument count
+ * @argv: this is an array of paramenters passed
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("the value of agrc is: %d\n", argc);
+	printf("the value of agrc is: %d\n", argc - 1);
 		return (0);
 }
