2c1ed7c20aec5ea4edaacb9e1a9e72fc81535bcc
StellaMighty <stellaikang@gmail.com>
2023-04-17 08:12:38 -0700

    a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File:4-sum.h
+ * Auth:Stella Ikang
+ * Desc:a function-like SUM(x, y) that computes the sum of the
+ * numbers x and y
+ *@x: a number to be  added
+ *@y: another number to be added
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
