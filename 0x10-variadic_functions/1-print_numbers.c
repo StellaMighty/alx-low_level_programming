80b5d3a77a475e4511ec7c0e3760c928385bf427
StellaMighty <stellaikang@gmail.com>
2023-04-20 21:45:58 -0700

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - this functioin prints numbers
+ * @separator: a pointer
+ * @n: numbers to be printed
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+}
